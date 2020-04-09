f1d901398a607552c0233222fce49e86e39bd1dd
Mihut Sorin <56602784+MihutSorin@users.noreply.github.com>
2020-04-09 18:34:22 +0300

    matrici

--- Program.cs
+++ Program.cs
@@ -1,63 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace operatii_numere_complexe
+namespace operatii_matrici
 {
+    class Operatii
+    {
+        int[,] a;
+        int[,] b;
+
+        public Operatii()
+        {
+        }
+
+        public Operatii(int[,]a, int[,]b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int[,]Suma(int[,]a,int[,]b)
+        {
+            int[,] res = new int[a.GetLength(0), a.GetLength(1)];
+                        for (int k = 0; k < a.GetLength(0); k++)
+                            for (int j = 0; j < a.GetLength(1); j++)
+                                res[k, j] = a[k, j] + b[k, j];
+
+                    return res;
+        }
+
+        public int[,] Multiply(int[,] a, int[,] b)
+        {
+            int rows = a.GetLength(0);
+            int size = b.GetLength(0);
+            int cols = b.GetLength(1);
+            int[,] m = new int[rows, cols];
+
+            for (int k = 0; k < rows; k++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int ret = 0;
+                    for (int x = 0; x < size; x++)
+                    {
+                        ret += a[k, x] * b[x, j];
+                    }
+                    m[k, j] = ret;
+                }
+            }
+            return m;
+        }
+    }
     class Program
     {
-        public class complex
+        static void Main(string[] args)
         {
-            // consideram numarul complex de forma algebrica z = x + y * i
-            public int x;
-            public int y;
-            public static complex suma(complex numarComplex1, complex numarComplex2)
+
+            int[,] a = {
+                        {1,2,3,4},
+                        {5,6,7,8},
+                        {9,1,2,3}
+                    };
+            int[,] b = {
+                        {1,2,3,4},
+                        {1,2,3,4},
+                        {1,2,3,4 } };
+            Operatii x = new Operatii();
+           print(x.Suma(a, b));
+            Console.WriteLine();
+            print(x.Multiply(a, b));
+            Console.ReadKey();
+        }
+
+        private static void print(int[,]a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
             {
-                complex sum = new complex();
-
-                sum.x = numarComplex1.x + numarComplex2.x;
-                sum.y = numarComplex1.y + numarComplex2.y;
-
-                return sum;
-            }
-            public static complex diferenta(complex numarComplex1, complex numarComplex2)
-            {
-                complex dif = new complex();
-                //z1-z2=(a1-a2)-(b1-b2)*i
-                dif.x = numarComplex1.x - numarComplex2.x;
-                dif.y = numarComplex1.y - numarComplex2.y;
-
-                return dif;
-            }
-            public static complex produsul(complex numarComplex1, complex numarComplex2)
-            {
-                complex prod = new complex();
-                //z1*z2=(a1a2-b1b2)+(a1b2+a2b1)*i
-                //(a+bi)(c+di) = (ac−bd) + (ad+bc)i
-                prod.x = (numarComplex1.x*numarComplex2.x)-(numarComplex1.y*numarComplex2.y);
-                prod.y = (numarComplex1.x*numarComplex2.y) + (numarComplex2.x*numarComplex2.y);
-
-                return prod;
-            }
-            static void Main(string[] args)
-            {
-                complex z, w, s,d,p;
-
-                z = new complex();
-                w = new complex();
-                s = new complex();
-                d = new complex();
-                p = new complex();
-                z.x = 3; z.y = 2;
-                w.x = 2; w.y = 2;
-
-                s = suma(z,w);
-                d = diferenta(z, w);
-                p = produsul(z, w);
-
-                Console.WriteLine("S = {0} + ({1} * i)", s.x.ToString(), s.y.ToString());
-                Console.WriteLine("D = {0} + ({1} * i)", d.x.ToString(), d.y.ToString());
-                Console.WriteLine("P = {0} + ({1} * i)", p.x.ToString(), p.y.ToString());
-                Console.ReadKey();
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
             }
         }
     }
 }
+
+
+
