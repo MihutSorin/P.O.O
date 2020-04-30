fd9df7b50eb2e903dd117a05508a64571459f6dd
Mihut Sorin <56602784+MihutSorin@users.noreply.github.com>
2020-04-30 13:40:56 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,88 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Text.RegularExpressions;
+using Microsoft.Win32.SafeHandles;
 
-
-namespace elevi
+namespace calendar
 {
-    class Elev
-    {
-        public string nume;
-        public string prenume;
-        public double media;
-        private int numar_note;
-        private double[] note;
-        internal static void loadFromFile(string fileName)
-        {
-            TextReader dataLoad = new StreamReader(fileName);
-            string buffer;
-            while ((buffer = dataLoad.ReadLine()) != null)
-            {
-                Console.WriteLine(buffer.ToLower());
-                
-            }
-        }
-        public Elev(string lastname, string firstname, int marks_number, double[] marks)
-        {
-            nume = lastname;
-            prenume = firstname;
-            media = 0;
-            numar_note = marks_number;
-            note = marks;
-
-            for (int i = 0; i < numar_note; i++)
-                media += note[i];
-
-            media /= numar_note;
-        }
-
-       
-    }
     class Program
     {
-
         static void Main(string[] args)
         {
+            List<Persoana> p = new List<Persoana>();
+            p.Add(new Persoana("mihut", "sorin",
+                "13.04", "mihut@gmail.com"));
            
-          // Elev.loadFromFile(@"..\..\data.txt");
-            StreamReader sr = new StreamReader(@"..\..\data.txt");
-            string linii = sr.ReadToEnd();
-            Console.WriteLine(sr.ReadToEnd());
-            Capitalizat(linii);
-            Console.WriteLine();
-           // string line = sr.ReadToEnd();
-            string[] s = linii.Split(' ');
-            List<string> x = new List<string>();
-            foreach (string a in s)
-            {
-                x.Add(a);
-              
-            }
-           
-            x.Sort();
-
-            List<int> y= new List<int>();
-            foreach (int b in linii)
-            {
-                y.Sort();
-
-            }
-
-            y.Sort();
+            p.Add(new Persoana("ciucle", "ioana", "13/12",
+                "ioan@gmail.com"));
+            p.ForEach(delegate (Persoana a
+                ) { Console.WriteLine($"{a.nume} {a.prenume}" +
+                    $" {a.datanasterii} {a.email}"); });
             Console.ReadKey();
-        }
-        internal static void Capitalizat(string line)
-        {
-            Char[] ca = line.ToCharArray();
-
-            foreach (Match m in Regex.Matches(line, @"\b[a-z]"))
-            {
-                ca[m.Index] = Char.ToUpper(ca[m.Index]);
-            }
-
-            Console.WriteLine(new string(ca));
         }
     }
 }
